c2b41a70f91cbdb22a8eba1af920a57bfc51f49d
Peiran <83073736+Peiran-WANG@users.noreply.github.com>
2022-07-03 19:55:22 +0200

    Update SHM_DSS.py

--- SHM_DSS.py
+++ SHM_DSS.py
@@ -119,8 +119,9 @@ class AHP:
 
     def run(self):
         max_eigen, CR, criteria_eigen = self.cal_weights(self.criteria)
+        critera_eigen_f = [float('{:.4f}'.format(i)) for i in criteria_eigen]
         st.write('**Criterion Layer**')
-        st.write('Criterion Layer Weight = {}\n'.format(criteria_eigen))  #格式化输出
+        st.write('Criterion Layer Weight = {}\n'.format(criteria_eigen_f))  #格式化输出
         st.write('Maximum Eigenvalue = {:<5f}'.format(max_eigen))
         st.write('Relative consistency index (CR) = {:<5f}'.format(CR))
         if CR < 0.1:
@@ -129,9 +130,6 @@ class AHP:
             st.error('Consistency check failed, please change the weights')
             return
             
-
-        st.write('Criterion Layer Weight = {:<5f}\n'.format(criteria_eigen))  #格式化输出
-
         max_eigen_list, CR_list, eigen_list = [], [], []
         for i in self.b:
             max_eigen, CR, eigen = self.cal_weights(i)
